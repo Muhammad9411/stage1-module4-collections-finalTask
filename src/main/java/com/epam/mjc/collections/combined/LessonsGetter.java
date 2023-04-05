587ddedfaa3ed1d97f1904da90e0512112a6508a
MuhammadJon <mtjohn9411@gmail.com>
2023-04-05 15:32:45 +0500

    Stage 1. Final Tasks solution has been developed

--- src/main/java/com/epam/mjc/collections/combined/LessonsGetter.java
+++ src/main/java/com/epam/mjc/collections/combined/LessonsGetter.java
@@ -1,10 +1,22 @@
 package com.epam.mjc.collections.combined;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 public class LessonsGetter {
     public Set<String> getLessons(Map<String, List<String>> timetable) {
+
+        Set<String> result = new HashSet<>();
+
+        for (Map.Entry<String, List<String>> e : timetable.entrySet()) {
+            for (String s : e.getValue()) {
+                System.out.println(s);
+                result.add(s);
+            }
+        }
+
+        return result;
     }
 }
--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -1,9 +1,29 @@
 package com.epam.mjc.collections.combined;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class MapFromKeysCreator {
     public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
+
+        Map<Integer, Set<String>> result = new HashMap<>();
+
+        for (Map.Entry<String, Integer> e : sourceMap.entrySet()) {
+            Set<String> set = new HashSet();
+            set.add(e.getKey());
+
+            if(result.containsKey(e.getKey().length())) {
+                Set<String> set1 = result.get(e.getKey().length());
+                set1.add(e.getKey());
+                result.remove(e.getKey().length());
+                result.put(e.getKey().length(), set1);
+                continue;
+            }
+            result.put(e.getKey().length(), set);
+        }
+
+        return result;
     }
 }
