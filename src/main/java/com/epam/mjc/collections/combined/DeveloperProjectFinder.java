bb9b15c23a3e68994686b422f4849b0033adf1d9
MuhammadJon <mtjohn9411@gmail.com>
2023-04-05 16:56:09 +0500

    Stage 1. Final Tasks solution has been developed

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -6,17 +6,21 @@ public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
 
         List<String> result = new ArrayList<>();
-
         for (Map.Entry<String, Set<String>> e : projects.entrySet()) {
-            for (String s: e.getValue()) {
-                if(developer.equals(s)) {
-                    result.add(e.getKey());
-                }
+            if (e.getValue().contains(developer)) {
+                result.add(e.getKey());
             }
         }
-
+        Collections.sort(result, new SortComparator());
         Collections.reverse(result);
 
         return result;
+    }
+
+    static class SortComparator implements Comparator<String> {
+        @Override
+        public int compare(String a, String b) {
+            return Integer.compare(a.length(), b.length());
+        }
     }
 }
