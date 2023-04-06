6e4a9dff393d23e52b906f2af4c4c647d76bffec
MuhammadJon <mtjohn9411@gmail.com>
2023-04-06 12:13:40 +0500

    Stage 1. Final Tasks bug fixed

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -6,31 +6,9 @@ public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
 
         List<String> result = new ArrayList<>();
-        Map<Integer, List<String>> m = new HashMap<>();
         for (Map.Entry<String, Set<String>> e : projects.entrySet()) {
-            List<String> set = new ArrayList<>();
             if (e.getValue().contains(developer)) {
-                set.add(e.getKey());
-                if (m.containsKey(e.getKey().length())) {
-                    List<String> set1 = m.get(e.getKey().length());
-                    set1.add(e.getKey());
-                    m.remove(e.getKey().length());
-                    m.put(e.getKey().length(), set1);
-                    continue;
-                }
-
-                m.put(e.getKey().length(), set);
-            }
-        }
-
-        for (Map.Entry<Integer, List<String>> e : m.entrySet()) {
-            if (e.getValue().size() > 1) {
-                List<String> nl = e.getValue();
-                Collections.sort(nl);
-                Collections.reverse(nl);
-                result.addAll(nl);
-            } else {
-                result.addAll(e.getValue());
+                result.add(e.getKey());
             }
         }
 
@@ -42,7 +20,14 @@ public class DeveloperProjectFinder {
     static class SortComparator implements Comparator<String> {
         @Override
         public int compare(String a, String b) {
-            return Integer.compare(b.length(), a.length());
+
+            int c = Integer.compare(b.length(), a.length());
+
+            if (c == 0) {
+                c = b.compareTo(a);
+            }
+
+            return c;
         }
     }
 }
